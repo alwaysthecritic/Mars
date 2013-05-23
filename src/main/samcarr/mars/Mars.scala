1ed3989fa7a9b8829ce9449abf579f000bb60be8
Sam Carr <samcarr@me.com>
2013-05-24 00:01:11 +0100

    Explicitly specifying UTF8 for input file, and fixed sample data to add in missing spaces.

--- src/main/samcarr/mars/Mars.scala
+++ src/main/samcarr/mars/Mars.scala
@@ -1,12 +1,14 @@
 package samcarr.mars
 
 import scala.io.Source
+import java.io.File
 
 object Mars {
     def main(args: Array[String]) {
         println("Welcome to Mars!")
         
         // Note that this will respect any line endings: /r, /n, /r/n.
-        val lines = Source.fromFile("qq").getLines
+        val lines = Source.fromFile("sampleData/sample1.txt", "UTF8").getLines
+        val config = new ConfigurationParser(lines).parse
     }
 }
