240218cbfb97816be15696a0ec13f2a3e102427b
Sam Carr <samcarr@me.com>
2013-05-27 23:00:08 +0100

    Cleaned up ConfigurationParser including deleting an unused method.

--- src/main/samcarr/mars/ConfigurationParser.scala
+++ src/main/samcarr/mars/ConfigurationParser.scala
@@ -3,12 +3,16 @@ package samcarr.mars
 import java.io.File
 import io.Source
 
-
 class BadConfigurationException(message: String) extends Exception(message)
 
 object ConfigurationParser {
   
     val MaxCommands = 100
+    
+    val GridDimensionsRegex = """^(\d{1,2}) (\d{1,2})$""".r
+    // Max two digits per coordinate as max coordinate is 50 (though we don't enforce that specifically).
+    val MissionStartRegex = """^(\d{1,2}) (\d{1,2}) ([NSEW])$""".r
+    val MissionCommandsRegex = """^([LRF]{0,%s})$""".format(MaxCommands).r
     
     // Accepting an iterator of the lines of config means we don't have to worry about line endings
     // and it's convenient for the caller to use Source.fromFile("foo").getLines
@@ -24,19 +28,16 @@ object ConfigurationParser {
     
     private def parseGridDimensions(line:String): (Int, Int) = {
         // Max two digits per dimension as max coordinate is 50 (though we don't enforce that specifically).
-        val lineFormat = """^(\d{1,2}) (\d{1,2})$""".r
         line match {
-            case lineFormat(maxX, maxY) => (maxX.toInt, maxY.toInt)
+            case GridDimensionsRegex(maxX, maxY) => (maxX.toInt, maxY.toInt)
             case _ => throw new BadConfigurationException("Couldn't parse Mars dimensions from line: " + line)
         }
     }
     
     private def parseMission(lines: Seq[String], maxX:Int, maxY:Int): Mission = {
         val firstLine = lines(0)
-        // Max two digits per coordinate as max coordinate is 50 (though we don't enforce that specifically).
-        val firstLineFormat = """^(\d{1,2}) (\d{1,2}) ([NSEW])$""".r
-        val (startX, startY, direction) = firstLine match {
-            case firstLineFormat(startX, startY, direction) => (startX.toInt, startY.toInt, Direction.withName(direction))
+        val (startX, startY, direction) = lines(0) match {
+            case MissionStartRegex(startX, startY, direction) => (startX.toInt, startY.toInt, Direction.withName(direction))
             case _ => throw new BadConfigurationException("Couldn't parse robot start position from line: " + firstLine)
         }
         
@@ -44,21 +45,11 @@ object ConfigurationParser {
             throw new BadConfigurationException("Robot start position out of bounds: " + firstLine)
         
         val secondLine = lines(1)
-        val secondLineFormat = """^([LRF]{0,%s})$""".format(MaxCommands).r
         val commands = secondLine match {
-            case secondLineFormat(commands) => commands
+            case MissionCommandsRegex(commands) => commands
             case _ => throw new BadConfigurationException("Commands not recognised (only LRF allowed) or too long (max %s): %s".format(MaxCommands, secondLine))
         }
         
         Mission(HappyRobot(startX, startY, direction), commands)
-    }
-    
-    private def parseRobotStart(line:String): (Int, Int) = {
-        // Allow max two digits per dimension as max coordinate is 50 (though we don't enforce that specifically).
-        val lineFormat = """^(\d{1,2}) (\d{1,2})$""".r
-        line match {
-            case lineFormat(width, height) => (width.toInt, height.toInt)
-            case _ => throw new BadConfigurationException("Couldn't parse Mars dimensions from line: " + line)
-        }
     }
 }
