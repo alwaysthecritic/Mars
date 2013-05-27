7d02a26adcd3445bb298e4d544bfe232eb65bccf
Sam Carr <samcarr@me.com>
2013-05-27 23:11:26 +0100

    Figured out how to setup Robot ADTs so fields don't get confused. Much better.

--- src/main/samcarr/mars/Robot.scala
+++ src/main/samcarr/mars/Robot.scala
@@ -1,13 +1,12 @@
 package samcarr.mars
 
-import scala.collection.mutable
 import Direction._
 
-// qq should these be vals? How does subclassing handle the fields?
-sealed abstract class Robot(val x: Int, val y: Int, val facing: Direction.Value)
+sealed abstract class Robot {
+    val x: Int
+    val y: Int
+    val facing: Direction.Value
+}
 
-// qq are all these override vals really the best way?
-//    perhaps ADTs are not really supposed to have shared state like this?
-case class HappyRobot(override val x: Int, override val y: Int, override val facing: Direction.Value) extends Robot(x, y, facing)
-
-case class LostRobot(override val x: Int, override val y: Int, override val facing: Direction.Value) extends Robot(x, y, facing)
+case class HappyRobot(val x: Int, val y: Int, val facing: Direction.Value) extends Robot
+case class LostRobot(val x: Int, val y: Int, val facing: Direction.Value) extends Robot
