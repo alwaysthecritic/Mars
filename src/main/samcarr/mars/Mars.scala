4156fd210cf995b364219e607c16ca98e4da09bf
Sam Carr <samcarr@me.com>
2013-05-28 08:44:22 +0100

    Taking file args on command line now, with more sophisticated error reporting too.

--- src/main/samcarr/mars/Mars.scala
+++ src/main/samcarr/mars/Mars.scala
@@ -1,6 +1,7 @@
 package samcarr.mars
 
 import scala.io.Source
+import java.io.File
 import java.io.PrintWriter
 import java.io.FileNotFoundException
 
@@ -12,17 +13,35 @@ object Mars {
     def main(args: Array[String]) {
         println("Welcome to Mars!")
 
-        val config = readConfig("sampleData/sample1.txt")
-        val finishedRobots = new MarsMissionRunner(config).runMissions()
-        writeOutput("sampleData/output1.txt", finishedRobots)
+        val inputFilePath = if (args.length >= 1) args(0) else "sampleData/sample1.txt"
+        val outputFilePath = if (args.length >= 2) args(1) else "sampleData/output1.txt"
+          
+        val config = readConfig(inputFilePath)
+        if (config.isDefined) {
+            val finishedRobots = new MarsMissionRunner(config.get).runMissions()
+            writeOutput(outputFilePath, finishedRobots)
+        }
     }
     
-    private def readConfig(filePath: String) = {
+    private def readConfig(filePath: String): Option[Configuration] = {
         println("Reading config from " + filePath)
         
-        // Note that this will respect any line endings: \r, \n, \r\n.
-        val configLines = Source.fromFile(filePath, FileEncoding).getLines
-        ConfigurationParser.parse(configLines)
+        val file = new File(filePath)
+        try {
+            // Note that this will respect any line endings: \r, \n, \r\n.
+            val configLines = Source.fromFile(file, FileEncoding).getLines
+            Some(ConfigurationParser.parse(configLines))
+        }
+        catch {
+           case e: FileNotFoundException => {
+               println("Couldn't find config file: " + file.getAbsolutePath())
+               None
+           }
+           case e: BadConfigurationException => {
+               println("Couldn't parse config: " + e.getMessage())
+               None
+           }
+        }
     }
     
     private def writeOutput(filePath: String, robots: Seq[Robot]) {
