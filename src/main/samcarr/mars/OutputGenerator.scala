2bac77ea15e372de13a08bb5360a731b49c4912d
Sam Carr <samuel_carr@mckinsey.com>
2013-06-04 13:10:35 +0100

    Slightly tighter syntax in OutputGenerator

--- src/main/samcarr/mars/OutputGenerator.scala
+++ src/main/samcarr/mars/OutputGenerator.scala
@@ -5,10 +5,8 @@ object OutputGenerator {
     // Given a list of Robots, generate String in the output format required, one line per Robot.
     def render(robots: Seq[Robot]): String = {
         val lines = robots.map {
-            _ match {
-              case HappyRobot(x, y, facing) => "%d %d %s".format(x, y, facing)
-              case LostRobot(x, y, facing) => "%d %d %s LOST".format(x, y, facing)
-            }
+            case HappyRobot(x, y, facing) => "%d %d %s".format(x, y, facing)
+            case LostRobot(x, y, facing) => "%d %d %s LOST".format(x, y, facing)
         }
         lines.mkString("\r\n")
     }
